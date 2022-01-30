8b87033335de5870295780842b864c60675c2a11
Ralphvandodewaard <rtvd@ziggo.nl>
2022-01-30 14:10:08 +0100

    Added scoring system

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,62 +1,82 @@
 <template>
   <div
     v-if="!loading"
-    class="flex flex-col flex-grow items-center gap-12 sm:gap-20 p-8 lg:px-40 lg:pt-32 lg:pb-20"
+    class="flex flex-col flex-grow gap-4 sm:gap-8 p-4 sm:p-8"
   >
-    <div class="flex flex-col items-center gap-8 w-3/5 lg:w-4/5 max-w-xl">
-      <div class="text-2xl text-center font-semibold text-white">
-        <h2 v-if="!selectedChampion">
-          Which champion says this voice line?
-        </h2>
-        <h2 v-if="selectedChampion">
-          The correct answer is <span :class="isCorrect ? 'text-green' : 'text-red'">{{ activeChampion.name }}</span>!
-        </h2>
-      </div>
-      <div
-        :class="selectedChampion ? `${isCorrect ? 'border-green' : 'border-red'} duration-500` : 'border-white'"
-        class="relative flex justify-center items-center w-full h-40 p-4 border-2 rounded shadow"
-      >
-        <div
-          :style="backgroundImageStyle"
-          :class="`${selectedChampion ? 'opacity-100 duration-500' : 'opacity-0'} ${backgroundImagePosition}`"
-          class="absolute inset-0 bg-cover"
-        />
-        <div class="flex flex-col items-center gap-2 z-10">
-          <p
-            :class="selectedChampion ? `${isCorrect ? 'text-green' : 'text-red'} text-shadow duration-500` : 'text-white'"
-            class="text-xl font-semibold text-center italic"
-          >
-            "{{ activeChampion.quotes.pick }}"
-          </p>
-          <div class="hidden">
-            <audio controls preload="auto">
-              <source type="audio/ogg" :src="audioSrc">
-              Your browser does not support the audio tag
-            </audio>
-          </div>
-        </div>
-        <button
-          @click="reset"
-          :disabled="!selectedChampion"
-          :class="selectedChampion ? `${isCorrect ? 'border-green' : 'border-red'} opacity-100 duration-200` : 'border-transparent cursor-default opacity-0'"
-          class="absolute -right-16 sm:-right-24 w-12 sm:w-16 h-12 sm:h-16 p-2 sm:p-3 border-2 rounded-full transform hover:scale-102 filter hover:brightness-125"
-        >
-          <img v-if="isCorrect" class="h-full w-full" src="../assets/icons/arrow-right.svg">
-          <img v-if="!isCorrect" class="h-full w-full" src="../assets/icons/reset.svg">
-        </button>
-      </div>
+    <div class="flex flex-col text-xl font-semibold text-white">
+      <p class="text-sm">
+        High score:
+        {{ highScore }}
+      </p>
+      <p>
+        Score:
+        {{ currentScore }}
+      </p>
     </div>
-    <div class="flex flex-col items-center gap-8 overflow-y-auto">
-      <SearchInput
-        v-model="searchInputValue"
-        @on-clear-input="clearInput"
-      />
-      <ChampionsList
-        :champions="champions"
-        :search-input-value="searchInputValue"
-        :is-selected="selectedChampion ? true : false"
-        @on-select-champion="selectChampion"
-      />
+    <div class="flex flex-col items-center gap-12 sm:gap-20 px-0 lg:px-32 pb-0 lg:pb-12 overflow-y-auto">
+      <div class="flex flex-col items-center gap-8 w-3/5 lg:w-4/5 max-w-xl">
+        <div class="text-2xl font-semibold text-white text-center ">
+          <h2 v-if="!selectedChampion">
+            Which champion says this voice line?
+          </h2>
+          <h2 v-if="selectedChampion">
+            The correct answer is <span :class="isCorrect ? 'text-green' : 'text-red'">{{ activeChampion.name }}</span>!
+          </h2>
+        </div>
+        <div
+          :class="selectedChampion ? `${isCorrect ? 'border-green' : 'border-red'} duration-500` : 'border-white'"
+          class="relative flex justify-center items-center w-full h-40 p-4 border-2 rounded shadow"
+        >
+          <div
+            :style="backgroundImageStyle"
+            :class="`${selectedChampion ? 'opacity-100 duration-500' : 'opacity-0'} ${backgroundImagePosition}`"
+            class="absolute inset-0 bg-cover"
+          />
+          <div class="flex flex-col items-center gap-2 z-10">
+            <p
+              :class="selectedChampion ? `${isCorrect ? 'text-green' : 'text-red'} text-shadow duration-500` : 'text-white'"
+              class="text-xl font-semibold text-center italic"
+            >
+              "{{ activeChampion.quotes.pick }}"
+            </p>
+            <div class="hidden">
+              <audio preload="auto">
+                <source type="audio/ogg" :src="audioSrc">
+                Your browser does not support the audio tag
+              </audio>
+            </div>
+          </div>
+          <button
+            @click="reset"
+            :disabled="!selectedChampion"
+            :class="selectedChampion ? `${isCorrect ? 'border-green' : 'border-red'} opacity-100 duration-200` : 'border-transparent cursor-default opacity-0'"
+            class="absolute -right-16 sm:-right-24 w-12 sm:w-16 h-12 sm:h-16 p-2 sm:p-3 border-2 rounded-full transform hover:scale-102 filter hover:brightness-125"
+          >
+            <img
+              v-if="isCorrect"
+              class="h-full w-full"
+              src="../assets/icons/arrow-right.svg"
+            >
+            <img
+              v-if="!isCorrect"
+              class="h-full w-full"
+              src="../assets/icons/reset.svg"
+            >
+          </button>
+        </div>
+      </div>
+      <div class="flex flex-col items-center gap-8 overflow-y-auto">
+        <SearchInput
+          v-model="searchInputValue"
+          @on-clear-input="clearInput"
+        />
+        <ChampionsList
+          :champions="champions"
+          :search-input-value="searchInputValue"
+          :is-selected="selectedChampion ? true : false"
+          @on-select-champion="selectChampion"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -91,6 +111,10 @@ export default class Home extends Vue {
   selectedChampion: Champion | null = null;
 
   searchInputValue = '';
+
+  highScore = 0;
+
+  currentScore = 0;
 
   get backgroundImageStyle(): any {
     const gradient = 'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5))';
@@ -146,7 +170,7 @@ export default class Home extends Vue {
   }
 
   get audioSrc(): string {
-    return `${this.communitydragonUrl}/11.23.1/champion/19/champ-select/sounds/choose`;
+    return `${this.communitydragonUrl}/11.23.1/champion/${this.activeChampion?.id}/champ-select/sounds/choose`;
   }
 
   get isCorrect(): boolean {
@@ -157,6 +181,7 @@ export default class Home extends Vue {
     this.loading = true;
     await this.fetchPatchVersion();
     await this.fetchChampions();
+    this.highScore = localStorage.getItem('highScore') ? Number(localStorage.getItem('highScore')) : 0;
     this.setActiveChampion();
     this.loading = false;
   }
@@ -190,6 +215,17 @@ export default class Home extends Vue {
 
   selectChampion(champion: Champion): void {
     this.selectedChampion = champion;
+    if (this.isCorrect) {
+      this.addScore();
+    }
+  }
+
+  addScore(): void {
+    this.currentScore = this.currentScore + 1;
+    if (this.currentScore > this.highScore) {
+      this.highScore = this.currentScore;
+      localStorage.setItem('highScore', this.highScore.toString());
+    }
   }
 
   clearInput(): void {
@@ -197,6 +233,9 @@ export default class Home extends Vue {
   }
 
   reset(): void {
+    if (!this.isCorrect) {
+      this.currentScore = 0;
+    }
     this.selectedChampion = null;
     this.searchInputValue = '';
     this.setActiveChampion();
