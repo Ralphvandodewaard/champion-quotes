6613f1ff918919328ce34c1ed7798ad19386740b
Ralphvandodewaard <rtvd@ziggo.nl>
2022-01-30 16:58:11 +0100

    Added changes for deployment

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,23 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+    meta: {
+      title: 'League Champion Quotes'
+    }
   }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+});
+
+router.beforeEach(async(to) => {
+  document.title = to.meta.title ? to.meta.title as string : 'League Champion Quotes';
 });
 
 export default router;
